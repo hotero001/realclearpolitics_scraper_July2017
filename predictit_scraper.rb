a502e93f9b2a192b198a290b01a0855dbcc32e70
Hector Otero <otero111@hotmail.com>
2017-07-08 22:54:24 -0500

    removed unnecessary lines of code, and added improved comments. I also included some TODOs for later

--- predictit_scraper.rb
+++ predictit_scraper.rb
@@ -1,5 +1,6 @@
-#script for scraping from predictit
+#script for scraping from RealClearPolitics
 
+#gems required for this to work
 require 'nokogiri'
 require 'httparty'
 require 'open-uri'
@@ -14,19 +15,23 @@ while true do
   approval_rating_to_float = []
 
   tr.each do |tr|
+    #this searches for the specific tr class where the polling data is located
     if (tr['class'] == 'rcpAvg')
-  	  #converts the scraped data to a string from a nokogiri element
-  	  #a = tr.to_s
-  	  #puts tr
+      #converts the scraped data to a string from a nokogiri element
       scraped_data_to_string = tr.to_s
-  	  approval_rating_to_float << scraped_data_to_string[109..112].to_f
-      #puts tr.at_css("td")
+
+      #TODO: The way we search for the data sucks. we need to write a better way
+      #to retrieve this information.
+      approval_rating_to_float << scraped_data_to_string[109..112].to_f
     end
   end
 
-  #here is where we would send a text message to our phone letting us know
-  #that the data has been updated, thus prompting us to execute a trade, hopefully
+  #this checks if the data has changed.
+  #TODO: Write a better way to check for the change in the polling data.
+  #This requires a rewrite whenever a new poll is released. Requires automation.
   if approval_rating_to_float[1] != 39.9
+    #TODO: Add code that would send our phone number a text message alert
+    #I wonder if this could be done using WhatsApp, thus avoiding charges with Twilio.
     puts "the data has changed. Determine whether or not it would be prudent to execute a trade"
   end
 end
